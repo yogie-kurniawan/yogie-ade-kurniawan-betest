cf6725b0def1bb16a18ac3bf40fb3a8686baf39e
yogie-kurniawan <yogieadekurniawan@gmail.com>
2024-11-10 07:30:22 +0700

    fix: database connection

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async (url) => {
   try {
-    mongoose.connect(url);
+    await mongoose.connect(url);
   } catch (error) {
     console.log(error.message);
   }
